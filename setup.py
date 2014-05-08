939d391b4ec9e8eb5e005b1aca0a582a45046ea1
amcanadas <amcanadas@gmail.com>
2014-05-08 19:54:24 +0200

    make openshift install requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+
 
 setup(name='YourAppName',
       version='0.1.0',
@@ -6,7 +8,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.6',
-            'psycopg2',
-        ],
+      install_requires=open('%s/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR')).readlines(),
 )
